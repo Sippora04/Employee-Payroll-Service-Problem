96a752f0d02cc9e2d820556698942bd76804bd78
Sippora04 <sippora.toppo95@gmail.com>
2020-11-09 00:00:23 +0530

    Added: store employee payroll into a file

---
+++ test/java/com/problem16/EmployeePayrollServiceTest.java
@@ -0,0 +1,20 @@
+package com.problem16;
+
+import java.util.Arrays;
+import org.junit.Test;
+import com.problem16.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEnteries() {
+		EmployeePayrollData[] arrayOfEmps= {
+				new EmployeePayrollData(1,"Sippora Toppo", 350000.0),
+				new EmployeePayrollData(2,"Asher David", 400000.0),
+				new EmployeePayrollData(3,"Akshay Nikam", 500000.0)
+		};
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+	}
+}
